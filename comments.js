b5db7ec12769c5e36e9127d0eea5caefba6529bc
Swatboy76 <lukenodarshvili@gmail.com>
2023-12-07 01:46:17 +0000

    worked on placeholder comment and JS

--- comments.js
+++ comments.js
@@ -1,7 +1,31 @@
 console.log("debug_0");
+//fishish "Load comments" Asap (preferably on the train).If you have energy left you can do make comments aswell.(dont kill yourself)
 
-let comments = [{ name: "Bobert" }];
+let comments = [{ name: "Bobert  bobybob", date: "02/17/2021", pfp: "N/A" }];
 
 function mAkeComment() {}
 
 function lOadComments() {}
+
+/*
+
+  myAppointmentsEl.innerHTML = "";
+  
+  for (let i = 0; i < appointments.length; i++) {
+    //it looks at how long the list is and goes though each entity
+    const cardEl = document.createElement("article"); // this part... makes an <article> and adds the class = "Appointment"
+    cardEl.classList.add("appointment");
+
+    const headingEl = document.createElement("h3"); // this one makes a H3 and puts the i inside... AH HA
+    headingEl.innerText = appointments[i].name; // They take the NUMBER that I is. and go into the list looking for the INDEX...
+    //they then put the "name" part of that index in the H3
+
+    const dateEl = document.createElement("span"); // they look to be doing a similar thing here and using a span (???(?))
+    dateEl.innerText = appointments[i].date;// the "span" name is more a missnomer. their the same. see nots for details
+
+    cardEl.appendChild(headingEl); //this where they add the two parts to the card.a flex then makes it flat with a gap imbetween
+    cardEl.appendChild(dateEl);
+
+    myAppointmentsEl.appendChild(cardEl);
+  } // do not use above code, only reverce engineer and understand (Comment the [Sailor mouth] out of it)
+*/
